ccc9ab7d5d982ffd05cb4d713f6af03042b72e33
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2024-05-17 22:15:57 +0900

    Add files via upload

---
+++ first.asm
@@ -0,0 +1,78 @@
+%macro printf 2
+   mov   eax, 4     ;system call number (4=sys_write)
+   mov   ebx, 1     ;file descriptor (stdout)
+   mov   ecx, %1    ;message to write
+   mov   edx, %2    ;message length
+   int	0x80        ;call kernel
+%endmacro
+
+;;reference https://www.tutorialspoint.com/assembly_programming/assembly_quick_guide.htm
+
+;;=======================================================
+;section	.text
+;   global _start     ;must be declared for linker (ld)
+;
+;_start:	            ;tells linker entry point
+;   mov   ecx, [num1]
+;   cmp   ecx, [num2]
+;   jg    check_third_num
+;   mov   ecx, [num2]
+;
+;check_third_num:
+;
+;   cmp   ecx, [num3]
+;   jg    _exit
+;   mov   ecx, [num3]
+;
+;_exit:
+;
+;   mov   [largest], ecx
+;   printf msg, len
+;   printf largest, 2
+;
+;   mov	eax,1       ;system call number (sys_exit)
+;   int 0x80
+;
+;section	.data
+;   msg db "The largest digit is: ", 0xa
+;   len equ $- msg
+;   num1 dd '47'
+;   num2 dd '22'
+;   num3 dd '31'
+;
+;segment .bss
+;   largest resb 2
+
+;section	.text
+;   global _start        
+;	
+;_start:	            
+;   mov ecx,10
+;   mov eax, '1'
+;	
+;l1:
+;   push ecx
+;   mov [num], eax
+;   printf num, 1
+;	
+;   mov eax, [num]
+;   ;inc eax
+;   pop ecx
+;   loop l1
+;	
+;   mov eax,1             ;system call number (sys_exit)
+;   int 0x80              ;call kernel
+;section	.bss
+;num resb 1
+
+section	.text
+   global _start        
+	
+_start:	            
+    printf num, 2
+
+    mov eax, 1
+    int 0x80
+    
+section .data
+num dd '11'
